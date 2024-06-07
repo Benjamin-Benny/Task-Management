32165d783600ddb58e16e533a97d4e57d7aff843
Benjamin Benny <35957758+Benjamin-Benny@users.noreply.github.com>
2024-06-07 22:14:27 +0530

    starter files second commit

--- backend/src/test/java/com/project/task_manager/TaskManagerApplicationTests.java
+++ backend/src/test/java/com/project/task_manager/TaskManagerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.project.task_manager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class TaskManagerApplicationTests {
 
 	@Test
 	void contextLoads() {
